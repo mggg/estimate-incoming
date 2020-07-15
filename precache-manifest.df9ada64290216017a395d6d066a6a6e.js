d34d1fe9c226e300636f2e1d2ff2ca01cc1e30f1
Gabe Schoenbach <gabe.schoenbach@gmail.com>
2020-07-15 12:35:55 -0500

    Updates

---
+++ precache-manifest.df9ada64290216017a395d6d066a6a6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20bf0486c83b056e19fbc0cb69a1b53a",
+    "url": "/estimate-incoming/index.html"
+  },
+  {
+    "revision": "9a4704957383b4ed9b5a",
+    "url": "/estimate-incoming/static/css/main.645df1f5.chunk.css"
+  },
+  {
+    "revision": "3c94a8a9ef42e1e3f53e",
+    "url": "/estimate-incoming/static/js/2.97bf280d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/estimate-incoming/static/js/2.97bf280d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a4704957383b4ed9b5a",
+    "url": "/estimate-incoming/static/js/main.19e0fe68.chunk.js"
+  },
+  {
+    "revision": "dc6f9f4273c76c0c3ae1",
+    "url": "/estimate-incoming/static/js/runtime-main.00015c1b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/estimate-incoming/precache-manifest.bb93b1aa7a812ea91cb7de8a2a20457e.js"
+  "/estimate-incoming/precache-manifest.df9ada64290216017a395d6d066a6a6e.js"
 );
 
 self.addEventListener('message', (event) => {
