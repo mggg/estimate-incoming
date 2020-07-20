803a2ad3619a17b2e465aa8cbee5413c88fe623a
Gabe Schoenbach <gabe.schoenbach@gmail.com>
2020-07-20 15:26:31 -0400

    add relative pathnames

--- src/Estimator.js
+++ src/Estimator.js
@@ -7,7 +7,7 @@ import {CSVLink} from "react-csv";
 
 const DownloadButton = props => {
   const downloadFile = () => {
-    window.location.href = "/estimate-incoming/template.csv"
+    window.location.href = window.location.pathname + "/template.csv"
   }
   return (
             <button className="btn btn-info" onClick={downloadFile}>
@@ -109,17 +109,18 @@ export default class Estimator extends React.Component {
   }
 
   componentDidMount() {
-    d3.csv('/estimate-incoming/IHME_pcts.csv').then(rows => {
+    console.log(window.location.pathname);
+    d3.csv(window.location.pathname + '/IHME_pcts.csv').then(rows => {
       rows.forEach(r => r.location_name = r.location_name.toLowerCase())
       ihme_df = new DataFrame(rows, [
         "location_name","lower_prob", "mean_prob", "upper_prob", "lower_prob_sept", "mean_prob_sept", "upper_prob_sept"]);
 
-      d3.csv('/estimate-incoming/MIT_pcts.csv').then(rows => {
+      d3.csv(window.location.pathname + '/MIT_pcts.csv').then(rows => {
         rows.forEach(r => r.location_name = r.location_name.toLowerCase())
         mit_df = new DataFrame(rows, [
           "location_name","lower_prob", "mean_prob", "upper_prob", "lower_prob_sept", "mean_prob_sept", "upper_prob_sept"]);
         // do this 2nd
-        fetch("/estimate-incoming/Tufts5470.csv").then(res => res.text()).then(data => {
+        fetch(window.location.pathname + "/Tufts5470.csv").then(res => res.text()).then(data => {
             let rows = d3.csvParseRows(data);
             rows.splice(0, 1);
             this.fileUploaded(rows);
